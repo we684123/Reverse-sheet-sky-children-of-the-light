87b4a22a116db595b21f44135ea184ae9e4f69b2
we684123 <we684123@gmail.com>
2020-12-16 20:03:46 +0800

    🎉 設定檔寫好了

---
+++ config/base.py
@@ -0,0 +1,60 @@
+def base():
+    return {
+        # 影片的位置
+        "video_path": "./sky.mkv",
+        # 譜面輸出目錄
+        "output_sheet_path": "./output",
+        # 譜面輸出名稱
+        "output_file_name": "output_sheet.txt",
+
+        # 用 get_boundart.py 轉換出來的圖，整體鍵盤的最左上邊界位置 (可用小畫家看)
+        # use get_boundart.py conversion video to an image
+        # in the image ,keyboard far left and far upper point position
+        "left_upper": [121, 530],
+        # 用 get_boundart.py 轉換出來的圖，整體鍵盤的最右下邊界位置 (可用小畫家看)
+        # use get_boundart.py conversion video to an image
+        # in the image ,keyboard far right and far lower point position
+        "right_lower": [623, 1400],
+
+        # 開始分析時間
+        "start_minute": 0,  # 只接受整數 # only integer
+        "start_second": 1,  # 只接受整數 # only integer
+        # 結束分析時間
+        "end_minute": 2,  # 只接受整數 # only integer
+        "end_second": 27,  # 只接受整數 # only integer
+
+        # 鍵盤格式。通常是5*3，像是鋼琴，少數是4*2，像是鼓
+        # keyboards format, like to piano is 5*3, and drum is 4*2
+        # 目前只支援這2種格式("5*3", "4*2")
+        # Currently only supports these 2 formats ("5*3", "4*2")
+        "keyboards_X_format": 5,  # only 5 or 4
+        "keyboards_y_format": 3,  # only 3 or 2
+        # 選擇你要輸出的譜面格式，能用的格式在下面 "sheet_formats" 中
+        # 如果不滿意預設格式可自己修改或新增
+        # 改了之後記得修改 "output_sheet_format" 內容
+        # choose output sheet format
+        # Available formats are below "sheet_formats"
+        # you can modify or add format
+        # Remember to modify "output_sheet_format" after changing
+        "output_sheet_format": "5_3_sheet_format_mode_1",
+        "sheet_formats": {
+            "5_3_sheet_format_mode_1": [
+                1, 2, 3, 4, 5,
+                6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15
+            ],
+            "5_3_sheet_format_mode_2": [
+                'A1', 'A2', 'A3', 'A4', 'A5',
+                'B1', 'B2', 'B3', 'B4', 'B5',
+                'C1', 'C2', 'C3', 'C4', 'C5'
+            ],
+            "4_2_sheet_format_mode_1": [
+                1, 2, 3, 4,
+                5, 6, 7, 8
+            ],
+            "4_2_sheet_format_mode_2": [
+                'A1', 'A2', 'A3', 'A4',
+                'B1', 'B2', 'B3', 'B4'
+            ]
+        },
+    }
