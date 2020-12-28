da58e06250a9c9ab654fc735a3febdc97728f488
we684123 <we684123@gmail.com>
2020-12-28 22:10:14 +0800

    🔧♻️ 3_generate_native_sheet、base 轉用 cool_down_frame

--- 3_generate_native_sheet.py
+++ 3_generate_native_sheet.py
@@ -21,6 +21,8 @@ with open(_temp, mode='r', encoding='utf-8') as f:
 analysis_from_video = json.loads(_analysis_from_video)
 frame_keyboards = json.loads(analysis_from_video['notes'])
 fps = analysis_from_video['fps']
+cool_down_time = rc['cool_down_time']  # 冷卻時間(單位 ms)
+cool_down_frame = round((cool_down_time/1000) / (1 / fps))  # 冷卻時間(單位 偵數)
 
 # 先來定義一下 kb_list 格式
 kb_list = []
@@ -47,7 +49,6 @@ for i in frame_keyboards:
 
 
 # 狀態器初始化
-refractory_time = rc['refractory_time']  # 冷卻時間(單位 偵數)
 temp_state_list = []  # 狀態器陣列
 for i in range(0, len(frame_keyboards[0])):
     temp_state = {
@@ -75,7 +76,7 @@ for n in range(0, len(kb_list)):
         # logger.debug('+')
         track = n
         after_time = (m - temp_state_list[track]['st_frame'])
-        refractory_timeout = after_time > refractory_time
+        refractory_timeout = after_time > cool_down_frame
         trigger = kb_list[track][m] < trigger_valve[n]
         if trigger and refractory_timeout:
             # logger.debug('.')
