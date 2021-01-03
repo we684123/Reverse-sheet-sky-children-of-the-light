86f9200d14b3f62eaa413978587dca12dc7a3a16
永格天 <we684123@gmail.com>
2021-01-03 20:01:42 +0800

    ✨ 透過 time sleep 控制播放速度，現在還是稍快，預計之後用pid解決

--- play_video.py
+++ play_video.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import time
 
 import cv2
 
@@ -13,6 +14,13 @@ if __name__ == '__main__':
     video_path = Path(rc['video_path'])
     cap = cv2.VideoCapture(str(video_path))
     frame_end = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    # 控制播放速度用
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    frame_time = 1 / fps
+    wait_time = 0
+    area_time = 0
+    now_time = time.time()
 
     # 再處理剩下的
     while cap.isOpened():
@@ -45,6 +53,17 @@ if __name__ == '__main__':
         video = ru.link_line(binary)
 
         cv2.imshow('Video Player', video)
+
+        # frame_time
+        area_time = time.time() - now_time
+        wait_time = abs(frame_time - area_time)
+        time.sleep(wait_time)
+        now_time = time.time()
+
+        print(f"area_time = {area_time} wait_time = {wait_time}")
+        if frame_count >= 500:
+            print(area_time)
+            break
 
         if cv2.waitKey(1) == ord('q'):
             break
