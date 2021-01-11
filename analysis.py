93bb35d1290a699d43e127ff67fe82b963fc1420
永格天 <we684123@gmail.com>
2021-01-11 16:41:36 +0800

    ♻️ analysis 現在終於可以稱得上完成了

--- analysis.py
+++ analysis.py
@@ -27,8 +27,6 @@ fps = analysis_from_video['fps']
 cool_down_time = rc['cool_down_time']  # 冷卻時間(單位 ms)
 cool_down_frame = round((cool_down_time / 1000) / (1 / fps))  # 冷卻時間(單位 偵數
 
-analysis_from_video = json.loads(_analysis_from_video)
-frame_keyboards = json.loads(analysis_from_video['notes'])
 
 # 先來定義一下 kb_list 格式
 kb_list = []
@@ -65,35 +63,39 @@ with open(_temp, mode='r', encoding='utf-8') as f:
 native_sheet = json.loads(_native_sheet)
 original_sheet = native_sheet['original_sheet']
 trigger_valve = native_sheet['trigger_valve']
+len_track = int(rc['keyboards_X_format']) * int(rc['keyboards_y_format'])
+# ===== 準備完畢 =====
 
 
-horizon_range = [280, 400]
-track = 9
-# now_frame = 500
-# now_frame_height = 800
+def generate_pixel_force():
+    # 因為 np.shape(kb_list) 結果會一樣，故直接用 0 取代
+    pixel_force = np.linspace(0, len(kb_list[0]), len(kb_list[0]))
+    return pixel_force
 
 
-def in_range(x):
-    if x['keyboard'] == track and x['type'] == 'note':
-        return x
+def generate_note_st_ed():
+    # 生出起始觸發時間 + 冷卻時間
+    note_st_ed_list = []
+    for track in range(0, len_track):
+        def is_note_from_track(x):
+            if x['keyboard'] == track and x['type'] == 'note':
+                return x
 
-
-rt = filter(in_range, original_sheet.copy())
-rt = list(rt)
-ironman = np.linspace(0, len(kb_list[track]), len(kb_list[track]))
-
-# 生出起始觸發時間 + 冷卻時間
-note_st_ed = np.zeros(len(kb_list[track]))
-for _i in rt:
-    _index = _i['frame']
-    _cd = _index + cool_down_frame
-    note_st_ed[_index:_cd] = trigger_valve[track]
+        rt = filter(is_note_from_track, original_sheet.copy())
+        rt = list(rt)
+        note_st_ed = np.zeros(len(kb_list[track]))
+        for _i in rt:
+            _index = _i['frame']
+            _cd = _index + cool_down_frame
+            note_st_ed[_index:_cd] = trigger_valve[track]
+        note_st_ed_list.append(note_st_ed)
+    return note_st_ed_list
 
 
 # horizon_range = [280, 400]
 # now_frame = 300
 # index_height = 800
-def check_graph(ironman, kb_list, note_st_ed,
+def check_graph(pixel_force, kb_list, note_st_ed,
                 track, horizon_range, now_frame, index_height):
     hr = horizon_range
 
@@ -102,22 +104,36 @@ def check_graph(ironman, kb_list, note_st_ed,
 
     fig = plt.figure(f'track{track}')
     plt.plot(
-        ironman[hr[0]:hr[1]], kb_list[track][hr[0]:hr[1]],
+        pixel_force[hr[0]:hr[1]], kb_list[track][hr[0]:hr[1]],
         color='#48D1CC', linestyle='solid', marker='.'
     )
     plt.plot(
-        ironman[hr[0]:hr[1]], note_st_ed[hr[0]:hr[1]],
+        pixel_force[hr[0]:hr[1]], note_st_ed[track][hr[0]:hr[1]],
         color='orange', linestyle='solid', marker='|'
     )
     plt.plot(
-        ironman[hr[0]:hr[1]], now_index[hr[0]:hr[1]],
+        pixel_force[hr[0]:hr[1]], now_index[hr[0]:hr[1]],
         color='red', linestyle='solid', marker='v'
     )
     fig.show()
     # input('1')
 
 
-image = check_graph(ironman, kb_list, note_st_ed, 9, [280, 400], 300, 800)
-image = check_graph(ironman, kb_list, note_st_ed, 3, [280, 400], 300, 800)
+# len(pixel_force)
+# len(kb_list[0])
+# len(note_st_ed_list)
+if __name__ == '__main__':
+    index_height = 800
+    pixel_force = generate_pixel_force()
+    note_st_ed_list = generate_note_st_ed()
+    now_frame = 600
+
+    horizon_range = [280, 400]
+    track = 9
+    image = check_graph(pixel_force, kb_list, note_st_ed_list,
+                        track, horizon_range, now_frame, index_height)
+    track = 3
+    image = check_graph(pixel_force, kb_list, note_st_ed_list,
+                        track, horizon_range, now_frame, index_height)
 
 #
