e23bcd7494cc5d437d533dbd7e659755b752b434
永格天 <we684123@gmail.com>
2021-06-13 01:32:23 +0800

    ✨🚧🐛 hsv_fix 新增 比例挑整、ROI區域。 修正 各類邊界溢出crash的問題

--- hsv_fix.py
+++ hsv_fix.py
@@ -19,7 +19,31 @@ def ng(x):  # nothing
     pass
 
 
-video_path = Path(rc['video_path'])  # "./sky.mkv"
+def remove_irrelevant(img, left: int, up: int, right: int, down: int):
+    if up >= down:
+        cv2.setTrackbarPos('up', _wn, up - 1)
+        up = down - 1
+    if left >= right:
+        cv2.setTrackbarPos('left', _wn, left - 1)
+        left = right - 1
+    return ru.get_crop_img(img, [left, up], [right, down])
+
+
+def img_resize(image, height_new, width_new):
+    height, width = image.shape[0], image.shape[1]
+    try:
+        if width / height >= width_new / height_new:
+            img_new = cv2.resize(
+                image, (width_new, int(height * width_new / width)))
+        else:
+            img_new = cv2.resize(
+                image, (int(width * height_new / height), height_new))
+    except Exception:
+        return image
+    return img_new
+
+
+video_path = Path(rc['video_path'])
 if not video_path.exists():
     logger.warning('video_path is not exist!')
     exit()
@@ -28,6 +52,8 @@ cap = cv2.VideoCapture(str(video_path))
 frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 duration = count / fps
 minute = int(duration / 60)
@@ -53,8 +79,18 @@ return_frame = int(lasting_seconds) * fps + specify_count
 
 # 調整視窗的名稱
 _wn = 'effect_config'
-cv2.namedWindow(_wn)
+cv2.namedWindow(_wn, 0)
 img_zeros = np.zeros((300, 512, 3), np.uint8)
+
+# 邊界、比例控制
+cv2.createTrackbar('up', _wn, 0, frame_height, ng)
+cv2.createTrackbar('down', _wn, frame_height, frame_height, ng)
+cv2.setTrackbarMin('down', _wn, 1)
+cv2.createTrackbar('left', _wn, 0, frame_width, ng)
+cv2.createTrackbar('right', _wn, frame_width, frame_width, ng)
+cv2.setTrackbarMin('right', _wn, 1)
+cv2.createTrackbar('scale', _wn, 400, frame_width, ng)
+cv2.setTrackbarMin('scale', _wn, 1)
 
 # RGB 顏色調整軸0~255
 cv2.createTrackbar('H_1_0', _wn, hsv['lower_yellow'][0], 179, ng)
@@ -87,6 +123,11 @@ while cap.isOpened():
         break
     if frame_count > return_frame:
         cap.set(cv2.CAP_PROP_POS_FRAMES, specify_count)
+    boundary_up = cv2.getTrackbarPos('up', _wn)
+    boundary_down = cv2.getTrackbarPos('down', _wn)
+    boundary_left = cv2.getTrackbarPos('left', _wn)
+    boundary_right = cv2.getTrackbarPos('right', _wn)
+    scale = cv2.getTrackbarPos('scale', _wn)
     H_1_0 = cv2.getTrackbarPos('H_1_0', _wn)
     S_1_0 = cv2.getTrackbarPos('S_1_0', _wn)
     V_1_0 = cv2.getTrackbarPos('V_1_0', _wn)
@@ -102,26 +143,28 @@ while cap.isOpened():
     binarization_thresh = cv2.getTrackbarPos('binarization_thresh', _wn)
     use_closing = cv2.getTrackbarPos('use_closing', _wn)
 
+    frame = remove_irrelevant(
+        frame,
+        boundary_left, boundary_up,
+        boundary_right, boundary_down
+    )
+
     lower_yellow = np.array([H_1_0, S_1_0, V_1_0])
     upper_yellow = np.array([H_1_1, S_1_1, V_1_1])
     lower_rad = np.array([H_2_0, S_2_0, V_2_0])
     upper_rad = np.array([H_2_1, S_2_1, V_2_1])
-
     mask, res = ru.get_keyboard_by_hsv(
         frame,
         lower_yellow, upper_yellow,
         lower_rad, upper_rad)
 
     img = ru.get_binary_img(res, binarization_thresh)
-    # (binary == mask).all()
-    # np.unique(binary)
-    # np.sum(binary)
-    # np.sum(mask)
+
     if int(use_closing) == 1:
         img = ru.link_line(img)
 
-    pic = cv2.resize(img, (960, 540), interpolation=cv2.INTER_CUBIC)
-    cv2.imshow('result', pic)
+    img = img_resize(img, scale, frame_width)
+    cv2.imshow('result', img)
 
     if cv2.waitKey(1) == ord('q'):
         break
